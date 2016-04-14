dac9dab36ec515bb89c3db9b977c417a08abda06
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-04-14 11:10:14 +0100

    simple filter representation

--- occult/filters.py
+++ occult/filters.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
-Filter = namedtuple('Filter', 'pivot fwhm')
+Filter = namedtuple('Filter', 'name pivot fwhm')
 
-sdss_filters = {'u': Filter(3540, 570), 'g': Filter(4770, 1370),
-                'r': Filter(6230, 1370), 'i': Filter(7630, 1530),
-                'z': Filter(9130, 950)}
+sdss_filters = {'u': Filter('u', 3540, 570), 'g': Filter('g', 4770, 1370),
+                'r': Filter('r', 6230, 1370), 'i': Filter('i', 7630, 1530),
+                'z': Filter('z', 9130, 950)}
+                
+ha = Filter('ha', 6565, 50)
